13750c645a70cac3890048d118de3fa2ac9d58c1
HectorVara <hvaram@fpcoruna.afundacion.org>
2022-09-27 09:42:07 +0200

    Ejercicio 6

--- sprint1/principal.py
+++ sprint1/principal.py
@@ -1,3 +1,4 @@
+import time
 from fibonacci2 import funcion_fibonacci2
 from Fibonacci import funcion_fibonacci
 print("""Qué método desea usar?
@@ -6,7 +7,16 @@ b) Fibonacci2""")
 opcion=input("Opción: ")
 if opcion=="a":
 	numero=input("Introduzca número: ")
+	start_time=time.time()
 	print(funcion_fibonacci(int(numero)))
+	end_time=time.time()
+	elapsed_time=end_time-start_time
+	print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
+	
 elif opcion=="b":
 	numero=input("Introduzca número: ")
+	start_time=time.time()
 	print(funcion_fibonacci2(int(numero)))
+	end_time=time.time()
+	elapsed_time=end_time-start_time
+	print('El tiempo de ejecución ha sido :' + str(elapsed_time) + ' s')
