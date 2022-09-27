503ece566fb9dfdc30ccdba5ff2b3adcd6a92a72
HectorVara <hvaram@fpcoruna.afundacion.org>
2022-09-27 09:37:34 +0200

    Ejercicio 5

--- sprint1/principal.py
+++ sprint1/principal.py
@@ -1,3 +1,12 @@
 from fibonacci2 import funcion_fibonacci2
-opcion=input("Introduzca número: ")
-print(funcion_fibonacci2(int(opcion)))
+from Fibonacci import funcion_fibonacci
+print("""Qué método desea usar?
+a) Fibonacci
+b) Fibonacci2""")
+opcion=input("Opción: ")
+if opcion=="a":
+	numero=input("Introduzca número: ")
+	print(funcion_fibonacci(int(numero)))
+elif opcion=="b":
+	numero=input("Introduzca número: ")
+	print(funcion_fibonacci2(int(numero)))
