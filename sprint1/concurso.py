f09529a9b916dce23ae5a084b19f185f36362215
HectorVara <hvaram@fpcoruna.afundacion.org>
2022-09-27 10:02:15 +0200

    Ejercicio 7

--- sprint1/concurso.py
+++ sprint1/concurso.py
@@ -1,62 +1,73 @@
-pregunta="""En qué año estamos?
+import random
+pregunta1="""En qué año estamos?
 a)2022
 b)2021
 c)1989
 
 """
-
-opcion= input(pregunta)
-puntos=0
-if opcion=="a":
-	print("Correcto")
-	puntos+=5
-elif opcion=="b":
-	print("Incorrecto")
-	puntos-=5
-elif opcion=="c":
-	print("Incorrecto")
-	puntos-=5
-else:
-	print("Teclea a,b o c")
-pregunta="""En qué año fue el mundial de Francia?
+pregunta2="""En qué año fue el mundial de Francia?
 a)2022
 b)2021
 c)1998
 
 """
-opcion= input(pregunta)
-
-if opcion=="a":
-	print("Incorrecto")
-	puntos+=5
-elif opcion=="b":
-	print("Incorrecto")
-	puntos-=5
-elif opcion=="c":
-	print("Correcto")
-	puntos-=5
-else:
-	print("Teclea a,b o c")
-	
-pregunta="""En qué año fue el mundial de Italia?
+pregunta3="""En qué año fue el mundial de Italia?
 a)2022
 b)2021
 c)1990
 
 """
-opcion= input(pregunta)
+puntos=0
+Preguntas=[pregunta1,pregunta2,pregunta3]
+cont=2
+while cont>0:
+	indice=random.randint(0,cont)
+	opcion= input(Preguntas[indice])
+	if indice==0:
+		Preguntas.pop(indice)
+		if opcion=="a":
+			print("Correcto")
+			puntos+=5
+		elif opcion=="b":
+			print("Incorrecto")
+			puntos-=5
+		elif opcion=="c":
+			print("Incorrecto")
+			puntos-=5
+		else:
+			print("Teclea a,b o c")
+			
+	if indice==1:
+		Preguntas.pop(indice)
+		if opcion=="a":
+			print("Incorrecto")
+			puntos-=5
+		elif opcion=="b":
+			print("Incorrecto")
+			puntos-=5
+		elif opcion=="c":
+			print("Correcto")
+			puntos+=5
+		else:
+			print("Teclea a,b o c")	
+	
+	if indice==2:
+		Preguntas.pop(indice)
+		if opcion=="a":
+			print("Incorrecto")
+			puntos-=5
+		elif opcion=="b":
+			print("Incorrecto")
+			puntos-=5
+		elif opcion=="c":
+			print("Correcto")
+			puntos+=5
+		else:
+			print("Teclea a,b o c")			
+		
+	cont-=1
+	
 
-if opcion=="a":
-	print("Incorrecto")
-	puntos-=5
-elif opcion=="b":
-	print("Incorrecto")
-	puntos-=5
-elif opcion=="c":
-	print("Correcto")
-	puntos+=5
-else:
-	print("Teclea a,b o c")
 	
 print(str(puntos)+" puntos")
 	
