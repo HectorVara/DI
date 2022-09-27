45571aabd2242d553a8578fac4c7505b94a60643
HectorVara <hvaram@fpcoruna.afundacion.org>
2022-09-27 08:39:57 +0200

    Ejercicio 1

---
+++ sprint1/concurso.py
@@ -0,0 +1,17 @@
+pregunta="""En qué año estamos?
+a)2022
+b)2021
+c)1989
+
+"""
+
+opcion= input(pregunta)
+
+if opcion=="a":
+	print("Correcto")
+elif opcion=="b":
+	print("Incorrecto")
+elif opcion=="c":
+	print("Incorrecto")
+else:
+	print("Teclea a,b o c")
