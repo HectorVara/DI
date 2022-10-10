106c2b466b68a6918a565f07a948e8e2aebd58cb
HectorVara <hvaram@fpcoruna.afundacion.org>
2022-10-10 16:34:34 +0200

    ejercicio 4

---
+++ extraPython1/ejercicio4.py
@@ -0,0 +1,8 @@
+def es_vocal(c):
+    if c=='a' or c=='e' or c=='i' or c=='o' or c=='u' or c=='A' or c=='E' or c=='I' or c=='O' or c== 'U':
+        return True
+    else:
+        return False
+
+
+print(es_vocal("a"))
