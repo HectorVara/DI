fa9170ddba8c8e0ef808efc5c7b5788d16e5bc8c
HectorVara <hvaram@fpcoruna.afundacion.org>
2022-11-09 11:03:09 +0100

    No logro enlazar el boton en CatalogFragment.java

--- sprint3android/mycatalog/app/src/main/java/com/example/myapplication/CatalogActivity.java
+++ sprint3android/mycatalog/app/src/main/java/com/example/myapplication/CatalogActivity.java
@@ -1,30 +1,123 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class CatalogActivity extends AppCompatActivity {
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class CatalogActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,
+        DrawerLayout.DrawerListener  {
     private Button boton;
     private Context context= this;
+    private DrawerLayout drawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        boton= findViewById(R.id.detalle);
-        boton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent myIntent = new Intent(context, DetailActivity.class);
-                context.startActivity(myIntent);
 
-            }
-        });
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(findViewById(R.id.toolbar));
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        MenuItem menuItem = navigationView.getMenu().getItem(0);
+        onNavigationItemSelected(menuItem);
+        menuItem.setChecked(true);
+
+    }
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int title;
+        switch (menuItem.getItemId()) {
+            case R.id.catalogo:
+                title = R.string.catalogo;
+                Fragment fragment = CatalogFragment.newInstance(getString(title));
+                getSupportFragmentManager()
+                        .beginTransaction()
+
+                        .replace(R.id.home_content, fragment)
+                        .commit();
+
+
+                setTitle(getString(title));
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+
+                break;
+            case R.id.about:
+                title = R.string.about;
+                Fragment aboutFragment = AboutFragment.newInstance(getString(title));
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.home_content, aboutFragment)
+                        .commit();
+
+
+                setTitle(getString(title));
+
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+
+                break;
+
+            default:
+                throw new IllegalArgumentException("menu option not implemented!!");
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+    }
+
+    @Override
+    public void onDrawerOpened(@NonNull View drawerView) {
+        Toast.makeText(this, getString(R.string.navigation_drawer_open),
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDrawerClosed(@NonNull View drawerView) {
+
+    }
+
+    @Override
+    public void onDrawerStateChanged(int newState) {
 
     }
 }
